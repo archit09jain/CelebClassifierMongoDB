c194f46d277961e16f8a8191b3cb10f774bdaee6
archit.j <archit.j@Apples-MacBook-Pro-4.local>
2017-08-16 17:08:26 +0530

    Dataset cropped Sample

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,44 +1,83 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import javax.imageio.ImageIO;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
 import jjil.core.*;
+
+import java.io.FilenameFilter;
 import java.util.List;
 
 
 public class Main {
 
 
-    public static void main(String[] args) throws Exception {
-     //   BufferedImage bi = ImageIO.read(new FileInputStream("/Users/archit.j/Desktop/mongoarchitj/src/main/java/5.jpg"));
-       // findFaces(bi, 1, 100, new File("./result.jpg")); // change as needed
+    public static void storeCroppedDataset(String sourcePath,String destinationPath,int index) throws Exception{
 
-        String src = "/Users/archit.j/Desktop/mongoarchitj/src/main/java/4.jpg";
-        String destination = "./result.jpg";
+        String src = sourcePath;
+        String destination = destinationPath;
 
         BufferedImage inputImage= ImageIO.read(new FileInputStream(src));
         DigitalImageProcessing dip = new DigitalImageProcessing(src);
         List<Rect> masks = dip.findFaces();
+        System.out.println("Masks Found");
         List<BufferedImage> extractedFaceImages = dip.getAllFaceImages(inputImage,masks);
-
-
-        //BufferedImage result = ImageIO.read(new FileInputStream("/Users/archit.j/Desktop/mongoarchitj/src/main/java/5.jpg"));
-        //BufferedImage dest = result.getSubimage(faces.get(0).getTopLeft().getX(),faces.get(0).getTopLeft().getY(),faces.get(0).getWidth(), faces.get(0).getHeight());
-
-        //ColorSpace cs = ColorSpace.getInstance(ColorSpace.CS_GRAY);
-        //ColorConvertOp op = new ColorConvertOp(cs, null);
-        //BufferedImage image = op.filter(dest, null);
+        System.out.println("Faces Extracted");
 
         extractedFaceImages = dip.convertToGrayScale(extractedFaceImages);
+        System.out.println("Converted to Grayscal");
+        System.out.println(extractedFaceImages.size());
 
         //save data yourself
-
         for(int i = 0;i<extractedFaceImages.size();i++) {
 
-            String outputPath = "image" + i + ".jpg";
+            File directory = new File(destination);
+            if(!directory.exists())
+                directory.mkdir();
+
+            String outputPath =  destinationPath + "/" + index  + i + ".jpg";
             System.out.println(outputPath);
             File outputfile = new File(outputPath);
+            if(!outputfile.exists())
+                outputfile.createNewFile();
+            System.out.println(outputPath);
             ImageIO.write(extractedFaceImages.get(i), "jpg", outputfile);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        File dir = new File("/Users/archit.j/Desktop/dataset");
+        String[] dirList = dir.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File current, String name) {
+                return new File(current, name).isDirectory();
+            }
+        });;
+
+        for(String directories: dirList) {
+
+
+            String path = "/Users/archit.j/Desktop/dataset/" + directories;
+            File innerdir = new File(path);
+
+            File[] files = innerdir.listFiles(new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                         return name.toLowerCase().endsWith(".jpg");
+                    }
+                });
+
+            for(int i = 0;i<Math.min(files.length,50);i++) {
+                File file = files[i];
+                System.out.println(file);
+                String destination = "/Users/archit.j/Desktop/dataset2/" + directories;
+                String source = file.getAbsolutePath();
+                System.out.println(source);
+                storeCroppedDataset(source,destination,i);
+                System.out.println("DONE");
+            }
         }
     }
 
